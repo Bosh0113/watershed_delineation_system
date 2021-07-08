f3f3e427bb9b96f8430a0cc592dc0624e5b814ea
BeichenZhang <zbc555day@163.com>
2021-07-08 12:46:52 +0800

    流域范围查询功能

--- back_server.py
+++ back_server.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pymongo
+
+
 app = Flask(__name__)
+myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+mydb = myclient["BasinsDataBase"]
+mycol = mydb["wholeBasinScale"]
+
 
 @app.route('/main')
 def mainpage():
     return app.send_static_file("main.html")
+
+
+@app.route('/basins/queryScope', methods=['GET'])
+def queryScope():
+    lon = float(request.args.get('lon'))
+    lat = float(request.args.get('lat'))
+    clickQuery = {"features.geometry":{"$geoIntersects":{"$geometry":{"type": "Point","coordinates": [lon, lat]}}}}
+    geoJson = mycol.find_one(clickQuery)
+    if geoJson != None:
+        del geoJson["_id"]
+        return geoJson
+    else:
+        return '0'
+
 
 if __name__ == "__main__":
     app.run()
